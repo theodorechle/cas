e9c6d0159f34dc2d95c0b079f1d5ef19cc1b2140
Théodore <theodore.chailloleauleclerc@gmail.com>
2024-06-03 11:33:50 +0200

    Added var addition

--- version_cpp/src/addition.cpp
+++ version_cpp/src/addition.cpp
@@ -1,14 +1,12 @@
 #include "addition.hpp"
 #include "number.hpp"
+#include "multiplication.hpp"
 
 bool Addition::solve(bool *delete_self) {
     Node *child1 = getChild();
     Node *child2 = child1->getNext();
     Token typeFirstChild = child1->getTokenType();
     Token typeSecondChild = child2->getTokenType();
-    if (isOneTokenTheSame(typeFirstChild, typeSecondChild, Token::Function)) return false;
-    if (isOneTokenTheSame(typeFirstChild, typeSecondChild, Token::Variable) &&
-        isOneTokenTheSame(typeFirstChild, typeSecondChild, Token::Number)) return false;
     if (typeFirstChild == Token::Number && typeSecondChild == Token::Number) {
         Number *c1 = dynamic_cast<Number*>(child1);
         Number *c2 = dynamic_cast<Number*>(child2);
@@ -17,6 +15,17 @@ bool Addition::solve(bool *delete_self) {
         delete c2;
         *delete_self = true;
         return true;
+    }
+    else if (typeFirstChild == Token::Variable && typeSecondChild == Token::Variable) {
+        if (*child1 == *child2) {
+            Node *newNode = new Multiplication;
+            newNode->appendChild(new Number{"2"});
+            newNode->appendChild(child2);
+            child1->setNext(nullptr);
+            replaceChild(child1, newNode);
+            *delete_self = true;
+            return true;
+        }
     }
     return false;
 }
